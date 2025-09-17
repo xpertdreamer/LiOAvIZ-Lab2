e57ce2938cbc61ae9ca4f198e2cc8c2738e0757d
hellabastard <alexejbudnikow1810@gmail.com>
2025-09-17 12:39:21 +0300

    new graph script

--- scripts/graph.py
+++ scripts/graph.py
@@ -4,23 +4,19 @@ from scipy.interpolate import make_interp_spline
 
 # Данные
 sizes = np.array([100, 200, 300, 400, 1000, 2000, 4000, 10000])
-times = np.array([ 0.000573, 0.003869, 0.012829, 0.033208, 1.067853, 34.219271, 449.660521, 7409.165449])
+times = np.array([0.000573, 0.003869, 0.012829, 0.033208, 1.067853, 34.219271, 449.660521, 7409.165449])
 
 plt.figure(figsize=(10, 6))
 
-# Сглаживание линии
-sizes_smooth = np.linspace(sizes.min(), sizes.max(), 500)  # новые точки для плавности
-spl = make_interp_spline(sizes, times, k=3)  # кубическая сплайн-интерполяция
-times_smooth = spl(sizes_smooth)
-
-# Плавная линия
-plt.plot(sizes_smooth, times_smooth, color='b', label='Время умножения (плавно)')
+# Обычная линейная интерполяция для основных данных (соединяем точки прямыми линиями)
+plt.plot(sizes, times, color='b', label='Время умножения')
 
 # Оригинальные точки
 plt.scatter(sizes, times, color='b', edgecolor='k', zorder=5, label='Измеренные точки')
 
-# Плавная линия n^3
+# Сглаженная линия n^3
 c = times[-1] / (sizes[-1]**3)
+sizes_smooth = np.linspace(sizes.min(), sizes.max(), 500)  # новые точки для плавности
 plt.plot(sizes_smooth, c * sizes_smooth**3, linestyle='--', color='r', label=r'$N^3$ (масштабировано)')
 
 # Подписи и оформление
